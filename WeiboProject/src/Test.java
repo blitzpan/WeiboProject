4f0f306c24587c433e80aad77cd5c73442c57942
pyk <panyk@hdsxtech.com>
2015-12-31 14:06:36 +0800

    ...

--- WeiboProject/src/Test.java
+++ WeiboProject/src/Test.java
@@ -1,10 +1,28 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class Test {
-
+	public void fun(){
+		try {
+			String url = "http://vip.stock.finance.sina.com.cn/mkt/#sge_gold";
+			Document doc = Jsoup.connect(url).get();
+			System.out.println(doc.text());
+			System.out.println("--------------------------------------------");
+			if(doc.text().contains("tbl_wrap")){
+				System.out.println("contains");
+			}
+			Elements newsHeadlines = doc.select("#tbl_wrap");
+			System.out.println(newsHeadlines.text());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
-		Thread t = new Thread();
-		ThreadGroup tg = t.getThreadGroup();
-		tg.get
+		Test t = new Test();
+		t.fun();
 
 	}
 
